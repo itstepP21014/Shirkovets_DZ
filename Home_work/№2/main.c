2c0ce6a81a9b7c749772a28ed54b9b28e8806d68
oldiback <oldiback@mail.ru>
2014-11-30 11:05:05 +0200

    Shirkovets_DZ

---
+++ Home_work/№2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b;
++   printf("Input two numbers:\n");
++   scanf("%d%d",&a,&b);
+    printf("7 + 5 = 12\n");
+    printf("7 - 5 = 2\n");
+    printf("7 * 5 = 35\n");
+    printf("7 / 5 = 1\n");
+    printf("7 %% 5 = 2\n");
+    return 0;
+}
